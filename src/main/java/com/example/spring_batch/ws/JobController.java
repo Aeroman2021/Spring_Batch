4517d0f5c0141009f4235df4f891951153a73229
m_malakouti <malakouti.aero@gmail.com>
2022-11-12 16:26:45 +0330

    Controller layer added.

---
+++ src/main/java/com/example/spring_batch/ws/JobController.java
@@ -0,0 +1,44 @@
+package com.example.spring_batch.ws;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    private Job job;
+
+
+    @PostMapping("/importCustomers")
+    public void importCSVToDataBase(){
+        JobParameters jobParameter = new JobParametersBuilder()
+                .addLong("start at",System.currentTimeMillis())
+                .toJobParameters();
+        try {
+            jobLauncher.run(job,jobParameter);
+        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+}
