6bef73b0615001b1c457980c9b8ee6e6584e0006
m_malakouti <malakouti.aero@gmail.com>
2022-11-12 16:21:22 +0330

    model layer created.

---
+++ src/main/java/com/example/spring_batch/model/Account.java
@@ -0,0 +1,39 @@
+package com.example.spring_batch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account_info")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @Column(name = "ACCOUNT_ID")
+    private int accountId;
+
+    @Column(name = "NUMBER")
+    private String number;
+
+    @Column(name = "ACCOUNT_TYPE")
+    private AccountType accountType;
+
+    @OneToOne
+    @Column(name = "CUSTOMER_ID")
+    private Customer customerId;
+
+    @Column(name = "LIMIT")
+    private Long limit;
+
+    @Column(name = "OPEN_DATE")
+    private String openDate;
+
+    @Column(name = "BALANCE")
+    private Long balance;
+
+}
---
+++ src/main/java/com/example/spring_batch/model/AccountType.java
@@ -0,0 +1,8 @@
+package com.example.spring_batch.model;
+
+public enum AccountType {
+    SAVINGS,
+    RECURRING_DEPOSIT,
+    FIXED_DEPOSIT
+
+}
