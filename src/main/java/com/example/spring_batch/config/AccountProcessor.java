64262ecafed645c2d1215a973f6c71b7aad12d74
m_malakouti <malakouti.aero@gmail.com>
2022-11-13 09:45:11 +0330

    Three methods added to validate account balance, number, and type.

--- src/main/java/com/example/spring_batch/config/AccountProcessor.java
+++ src/main/java/com/example/spring_batch/config/AccountProcessor.java
@@ -4,10 +4,43 @@ import com.example.spring_batch.model.Account;
 import com.example.spring_batch.model.AccountType;
 import org.springframework.batch.item.ItemProcessor;
 
+import java.util.function.Predicate;
+
+import static com.example.spring_batch.model.AccountType.RECURRING_DEPOSIT;
+import static com.example.spring_batch.model.AccountType.SAVINGS;
+
 public class AccountProcessor implements ItemProcessor<Account, Account> {
 
     @Override
     public Account process(Account account) throws Exception {
-        return account;
+        if(accountTypeIsValid(account) &&
+             accountNumberIsValid(account) &&
+              balanceIsValid(account) &&
+                accountIsNotNull(account))
+            return account;
+        else
+            return null;
     }
+
+    private boolean accountTypeIsValid(Account account){
+        AccountType accountType = account.getAccountType();
+        return (accountType == SAVINGS ||
+                accountType == RECURRING_DEPOSIT ||
+                accountType == RECURRING_DEPOSIT);
+    }
+
+    private boolean balanceIsValid(Account acc) {
+        return acc.getBalance() > acc.getLimit();
+    }
+
+    private boolean accountIsNotNull(Account account){
+        return account != null;
+    }
+    private boolean accountNumberIsValid(Account account){
+        String number = account.getNumber();
+        return (number.startsWith("0") &&
+                number.length()==10);
+    }
+
+
 }
