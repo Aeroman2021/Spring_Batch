40c1fa0dd40ae50c74535f600e884c7b185b0ca8
m_malakouti <malakouti.aero@gmail.com>
2022-11-13 09:42:41 +0330

    two method added to validate national ID and Date of birth of customers.

--- src/main/java/com/example/spring_batch/config/CustomerProcessor.java
+++ src/main/java/com/example/spring_batch/config/CustomerProcessor.java
@@ -7,6 +7,22 @@ public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
 
     @Override
     public Customer process(Customer customer) throws Exception {
+        if(nationalIdIsValid(customer) && dobIsValid(customer))
             return customer;
+        else
+            return null;
     }
+
+    private boolean nationalIdIsValid(Customer customer){
+        return customer.getNationalId().length() == 10;
+    }
+
+    private boolean dobIsValid(Customer customer){
+        String dob = customer.getDob();
+        String year = dob.substring(6);
+        int yob = Integer.parseInt(year);
+        return yob > 1995;
+    }
+
+
 }
