bc719dd0b811fd305ae3777a2b0a5d74b606b63a
malakouti.aero@gmail.com <malakouti.aero@gmail.com>
2022-11-15 23:42:00 +0330

    Repositories updated.

--- src/main/java/com/example/spring_batch/repository/AccountRepository.java
+++ src/main/java/com/example/spring_batch/repository/AccountRepository.java
@@ -1,9 +1,9 @@
 package com.example.spring_batch.repository;
 
-import com.example.spring_batch.model.Account;
+import com.example.spring_batch.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account,Integer> {
+public interface AccountRepository extends JpaRepository<BankAccount,Integer> {
 }
