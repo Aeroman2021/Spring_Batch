4748545978654a61d3e858f828e99aa149344693
m_malakouti <malakouti.aero@gmail.com>
2022-11-22 16:24:16 +0330

    A method is added to return AccountType.

--- src/main/java/com/example/spring_batch/model/AccountType.java
+++ src/main/java/com/example/spring_batch/model/AccountType.java
@@ -1,8 +1,19 @@
 package com.example.spring_batch.model;
 
+import javafx.scene.layout.Priority;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public enum AccountType {
     SAVINGS,
     RECURRING_DEPOSIT,
-    FIXED_DEPOSIT
+    FIXED_DEPOSIT;
 
+    public static AccountType of(String name) {
+        return Stream.of(values())
+                .filter(p -> p.name().equals(name))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
 }
