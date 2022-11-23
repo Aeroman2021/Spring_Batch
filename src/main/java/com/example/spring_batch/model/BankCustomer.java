583da51c81c6555bc3128d09aff7be413688f9c1
m_malakouti <malakouti.aero@gmail.com>
2022-11-23 16:44:00 +0330

    The class updated.

--- src/main/java/com/example/spring_batch/model/BankCustomer.java
+++ src/main/java/com/example/spring_batch/model/BankCustomer.java
@@ -1,6 +1,7 @@
 package com.example.spring_batch.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,13 +10,13 @@ import java.util.List;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "BANK_CUSTOMER")
 public class BankCustomer {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CUSTOMER_ID")
     private int customerId;
 
