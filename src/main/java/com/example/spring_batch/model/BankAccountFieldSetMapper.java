b970dea7008deb86b42e6a0d90ddc280f38e0e51
m_malakouti <malakouti.aero@gmail.com>
2022-11-23 16:42:12 +0330

    The class updated.

--- src/main/java/com/example/spring_batch/model/BankAccountFieldSetMapper.java
+++ src/main/java/com/example/spring_batch/model/BankAccountFieldSetMapper.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindException;
 
+import java.util.Optional;
+
 @Component
 public class BankAccountFieldSetMapper implements FieldSetMapper<BankAccount> {
 
@@ -17,15 +19,20 @@ public class BankAccountFieldSetMapper implements FieldSetMapper<BankAccount> {
 
     @Override
     public BankAccount mapFieldSet(FieldSet fieldSet) throws BindException {
-        return new BankAccount(fieldSet.readInt("accountId"),
-                fieldSet.readString("accountNumber"),
-                AccountType.of(fieldSet.readString("accountType")),
-                customerRepository.findById(fieldSet.readInt("customerId")).get(),
-                fieldSet.readLong("accountLimit"),
-                fieldSet.readString("accountOpenDate"),
-                fieldSet.readLong("accountCurrentBalance"));
 
+        Optional<BankCustomer> customer =
+                customerRepository.findById(fieldSet.readInt("customerId"));
 
+        if (customer.isPresent()){
+            return new BankAccount(fieldSet.readInt("accountId"),
+                    fieldSet.readString("accountNumber"),
+                    AccountType.of(fieldSet.readString("accountType")),
+                    customer.get(),
+                    fieldSet.readLong("accountLimit"),
+                    fieldSet.readString("accountOpenDate"),
+                    fieldSet.readLong("accountCurrentBalance"));
+        } else
+            return null;
     }
 
 
